076336b3d3a76cf6d840f3b98c4585a5cf96db4a
MaxMoskalenko <mx.msklnk@gmail.com>
2021-08-04 11:46:12 +0300

    ex04 solution

--- courses/cunix/ex04/src/my_atoi.c
+++ courses/cunix/ex04/src/my_atoi.c
@@ -1,29 +1,12 @@
 int my_atoi(const char *nptr){
     int sign = nptr[0] == '-' ? -1 : 1;
     int number = 0;
-    int string_size = my_strlen(nptr);
-    int digits_number = 0;
 
-    for (int i = 0; i < string_size; i++){
-        if( nptr[i] - '0' < 0 || nptr[i] - '0' > 9 ) continue;
-        number += (nptr[i] - '0') * pow(10, digits_number);
-        digits_number++;
+    for (int i = 0; nptr[i] != '\0'; i++){
+        if(nptr[i] - '0' < 0 || nptr[i] - '0' > 9) continue;
+        
+        number = number * 10 + (nptr[i] - '0');
     }
-    return sign * number;
-}
-
-unsigned int my_strlen(char *str)
-{
-    int index = 0;
-    while (str[index++] != '\0'){}
-
-    return index - 1;
-}
-
-int pow(int base, int power){
-    int result = 1;
-    for (int i = 0; i < power; i++){
-        result *= base;
-    }
-    return base;
+    number = sign * number;
+    return sign < 0 ? number % 2147483649 : number % 2147483648;
 }
