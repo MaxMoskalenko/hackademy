bc4291b41cae23bbce4955925e364ed3683a2abe
MaxMoskalenko <mx.msklnk@gmail.com>
2021-09-14 21:23:52 +0300

    Ex10 complete

--- courses/golang/ex10-workerpool/goroutines.go
+++ courses/golang/ex10-workerpool/goroutines.go
@@ -4,38 +4,40 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 )
 
 func Run(poolSize int) {
-	jobs := make(chan string, poolSize)
+	jobs := make(chan float64, poolSize)
 	var wg sync.WaitGroup
 	id := 1
 	scan := bufio.NewScanner(os.Stdin)
 	for scan.Scan() {
-		jobs <- scan.Text()
+		input := string(scan.Bytes())
+		f, err := strconv.ParseFloat(input, 64)
+		handleError(err)
+		jobs <- f
+
 		if id <= poolSize {
 			wg.Add(1)
-			go worker(id, jobs, &wg)
+			go workerTask(id, jobs, &wg)
 			id++
 		}
 	}
 	close(jobs)
 	wg.Wait()
+
 }
 
-func worker(id int, jobs <-chan string, wg *sync.WaitGroup) {
-	if len(jobs) > 0 {
-		fmt.Printf("worker:%d spawning\n", id)
-		for j := range jobs {
-			fmt.Printf("worker:%d sleep:%s\n", id, j)
-			t, err := time.ParseDuration(j + "s")
-			handleError(err)
-			time.Sleep(t)
-		}
-		fmt.Printf("worker:%d stopping\n", id)
+func workerTask(id int, jobs <-chan float64, wg *sync.WaitGroup) {
+	fmt.Printf("worker:%d spawning\n", id)
+	for job := range jobs {
+		fmt.Printf("worker:%d sleep:%.1f\n", id, job)
+		time.Sleep(time.Duration(int(job*1000)) * time.Millisecond)
 	}
+	fmt.Printf("worker:%d stopping\n", id)
 	wg.Done()
 }
 
@@ -43,5 +45,5 @@ func handleError(err error) {
 	if err == nil {
 		return
 	}
-	fmt.Println("ERROR: ", err.Error())
+	fmt.Println("ERROR: ", err)
 }
